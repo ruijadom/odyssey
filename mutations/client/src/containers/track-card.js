cfd64bbedb083aa56b51b4a8506cdeca7d348886
ruijadom <ruijadomingues@gmail.com>
2022-01-30 11:27:58 +0000

    fix: track-card

--- mutations/client/src/containers/track-card.js
+++ mutations/client/src/containers/track-card.js
@@ -3,24 +3,24 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from '@reach/router';
-import { gql, useMutation} from '@apollo/client'
+import { gql, useMutation } from '@apollo/client';
 
 /**
- * Mutation to increment a track´s numberOfViews
- * (expored for tests)
+ * Mutation to increment a track's number of views
+ * (exported for tests)
  */
- export const INCREMENT_TRACK_VIEWS = gql`
- mutation IncrementTrackViewsMutation($incrementTrackViewsId: ID!) {
-   incrementTrackViews(id: $incrementTrackViewsId) {
-     code
-     success
-     message
-     track {
-       id
-       numberOfViews
-     }
-   }
- }
+export const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViewsMutation($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
 `;
 
 /**
@@ -30,12 +30,13 @@ import { gql, useMutation} from '@apollo/client'
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
-  const [ incrementTrackViews ] = useMutation(INCREMENT_TRACK_VIEWS, {
-    variables: { incrementsTrackViewsId: id},
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    // to observe what the mutation response returns
     onCompleted: (data) => {
       console.log(data);
-    }
-  })
+    },
+  });
 
   return (
     <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
